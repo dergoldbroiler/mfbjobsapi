ee63ecaac305e43f30fbe1b8e71b175bae4efb7c
Björn Zschernack <bz@dergoldbroiler.de>
2018-07-26 14:52:18 +0200

    extended main class

--- wp-content/plugins/mfbjobsapi/includes/class-mfbjobsapi-plugin.php
+++ wp-content/plugins/mfbjobsapi/includes/class-mfbjobsapi-plugin.php
@@ -14,11 +14,36 @@ class MFBJOBSAPI {
         
         /*
         * SHORTCODES
-        ++ define all shortcodes here   
         */
     
         // SC to test class, output: Version of Plugin
         add_shortcode('MFBJOBSAPI_Version', array($this, 'get_mfjobsapi_version'));
+        
+        // SC to test jobs, output differs
+        add_shortcode('MFBJOBSAPI_alljobs', array($this, 'get_all_jobs'));
+        
+        
+        /*
+        * CONSTANTS
+        */
+        define('MFBJOBSAPI_SupplierID','V000311000');
+        define('MFBJOBSAPI_HiringOrg','TVS Personalservice GmbH');
+        define('MFBJOBSAPI_HiringOrgWeb','http://www.tvs-personalservice.de');
+        define('MFBJOBSAPI_HiringOrgEmail','info@tvspersonalservice.de');
+        define('MFBJOBSAPI_HiringOrgContactSalutation',1); // 1 = Sehr geehrter, 2 = Sehr geehrte, 3 = Sehr geehrte/r Herr / Frau
+        define('MFBJOBSAPI_HiringOrgContactName','Schmidt');
+        define('MFBJOBSAPI_HiringOrgContactStreet','Friedrich-Ebert-Straße 16');
+        define('MFBJOBSAPI_HiringOrgContactZip','06237');
+        define('MFBJOBSAPI_HiringOrgContactCity','Leuna');
+        define('MFBJOBSAPI_HiringOrgContactCountry','DE');
+        define('MFBJOBSAPI_HiringOrgContactRegion','14'); //14=Sachsen Anhalt, Bundesland ATTRIBUTE: zu entnehmen aus der Datei Regionen-VAM-Code
+        define('MFBJOBSAPI_NAICS','78.10.0'); //Code für Branche, nach WZ2008, für Personalvermittler -> 78.10.0 = Vermittlung von Arbeitskräften
+        define('MFBJOBSAPI_HiringOrgContactPhoneCode','+049');
+        define('MFBJOBSAPI_HiringOrgContactAreaCode','3461');
+        define('MFBJOBSAPI_HiringOrgContactPhoneNr','826990');
+        define('MFBJOBSAPI_HiringOrgContactMobileAreaCode','170');
+        define('MFBJOBSAPI_HiringOrgContactMobileNr','9669624');
+        
     }
     
     /*
@@ -26,6 +51,7 @@ class MFBJOBSAPI {
     */
     public function get_mfjobsapi_version() {
         echo '<h1>Version: '.PLUGIN_MFBJOBSAPI_VERSION.'</h1>';
+        echo '<h2>Supplier: '.MFBJOBSAPI_SupplierID.'</h2>';
     }
     
     
@@ -35,8 +61,16 @@ class MFBJOBSAPI {
     @returns: string    
     */
     
-    public function get_xml_head() {
-        
+    public function get_xml_head() {        
+        $xmlcontent = '<?xml version="1.0" encoding="UTF-8"?><!--XML-Beispieldatei von XMLSpy generiert v2008 rel. 2 sp1 (http://www.altova.com)-->         
+                        <HRBAXMLJobPositionPosting>
+                            <Header>
+		                      <SupplierId>'.MFBJOBSAPI_SupplierID.'</SupplierId>
+		                      <Timestamp>2001-12-17T09:30:47.0Z</Timestamp>
+		                      <Amount>100</Amount>
+		                      <TypeOfLoad>F</TypeOfLoad>
+	                       </Header>'; 
+        return $xmlcontent;
     }
     
     /*
@@ -45,7 +79,7 @@ class MFBJOBSAPI {
     */
     
     public function get_xml_footer() {
-        
+        return '</HRBAXMLJobPositionPosting>';
     }
     
     
@@ -57,6 +91,72 @@ class MFBJOBSAPI {
     */
     
     public function get_all_jobs() {
+        //get all jobs
+        $jobs = get_posts(array('post_type' => 'job_listing','posts_per_page' => 100));
+        
+        //var to fill xml data in, starting with header
+        $xmlcontent = MFBJOBSAPI::get_xml_head();
+        
+        // some vars
+        
+        //loop through jobs
+        foreach ( $jobs as $job ) {
+            $xmlcontent .= '<Data>
+                                        <JobPositionPosting>
+                                            <JobPositionPostingId>'.$job->ID.'</JobPositionPostingId>
+                                            <HiringOrg>
+                                                <HiringOrgName>'.MFBJOBSAPI_HiringOrg.'</HiringOrgName>
+                                                <HiringOrgId>'.MFBJOBSAPI_SupplierID.'</HiringOrgId>
+                                                <ProfileWebSite>'.MFBJOBSAPI_HiringOrgWeb.'</ProfileWebSite>
+                                                <HiringOrgSize>2</HiringOrgSize>
+                                                <Industry>
+                                                    <NAICS>'.MFBJOBSAPI_NAICS.'</NAICS>
+                                                </Industry>
+                                                <Contact>
+                                                    <Salutation>'.MFBJOBSAPI_HiringOrgContactSalutation.'</Salutation>
+                                                    <Title/>
+                                                    <GivenName/>
+                                                    <NamePrefix/>
+                                                    <FamilyName>'.MFBJOBSAPI_HiringOrgContactName.'</FamilyName>
+                                                    <PositionTitle/>
+                                                    <!--
+                                                        PostalAddress: Es darf entweder das Feld "StreetName" oder das Feld "PostOfficeBox" befüllt sein. Sind beide befüllt, wird das SteA abgewiesen.
+                                                    -->
+                                                    <PostalAddress>
+                                                        <CountryCode>'.MFBJOBSAPI_HiringOrgContactCountry.'</CountryCode>
+                                                        <PostalCode>'.MFBJOBSAPI_HiringOrgContactZip.'</PostalCode>
+                                                        <Region>'.MFBJOBSAPI_HiringOrgContactRegion.'</Region>
+                                                        <Municipality/>
+                                                        <District/>
+                                                        <AddressLine/>
+                                                        <StreetName>'.MFBJOBSAPI_HiringOrgContactStreet.'</StreetName>
+                                                        <PostOfficeBox/>
+                                                    </PostalAddress>
+                                                    <VoiceNumber>
+                                                        <IntlCode>'.MFBJOBSAPI_HiringOrgContactPhoneCode.'</IntlCode>
+                                                        <AreaCode>'.MFBJOBSAPI_HiringOrgContactAreaCode.'</AreaCode>
+                                                        <TelNumber>'.MFBJOBSAPI_HiringOrgContactPhoneNr.'</TelNumber>
+                                                    </VoiceNumber>
+                                                    <MobilNumber>
+                                                        <IntlCode>'.MFBJOBSAPI_HiringOrgContactPhoneCode.'</IntlCode>
+                                                        <AreaCode>'.MFBJOBSAPI_HiringOrgContactMobileAreaCode.'</AreaCode>
+                                                        <TelNumber>'.MFBJOBSAPI_HiringOrgContactMobileNr.'</TelNumber>
+                                                    </MobilNumber>
+                                                    <FaxNumber>
+                                                        <IntlCode>+0</IntlCode>
+                                                        <AreaCode>99999999999999</AreaCode>
+                                                        <TelNumber>9999999999999999999999999</TelNumber>
+                                                    </FaxNumber>
+                                                    <EMail/>
+                                                    <GeneralWebSite>ftp:// </GeneralWebSite>
+                                                </Contact>
+                                            </HiringOrg>'; 
+        } // end foreach-loop through jobs
+        
+        //get xml-footer
+        $xmlcontent .= MFBJOBSAPI::get_xml_footer();
+        echo "<form><textarea cols=100 rows=30>". $xmlcontent . "</textarea></form>";
+       // print_r($jobs);
         
     }
 
